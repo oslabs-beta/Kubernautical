07032270879a5ba9d92bd27feda0026a15123d36
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-01 19:18:12 -0500

    added controllers

--- client/components/3D/3DU.tsx
+++ client/components/3D/3DU.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { ForceGraph3D } from 'react-force-graph';
-import * as THREE from 'three';
-import * as d3 from 'd3';
+// import React, { useEffect, useState, useRef } from 'react';
+// import { ForceGraph3D } from 'react-force-graph';
+// import * as THREE from 'three';
+// import * as d3 from 'd3';
 // import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 // import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 // import ClickedNodeDetail from './detailOfClickedNode';
--- server/routers/apiRouter.ts
+++ server/routers/apiRouter.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import promRouter from './promRouter';
+import clusterRouter from './clusterRouter';
 
 router.use('/prom', promRouter);
+router.use('/cluster', clusterRouter)
 
 export default router;
