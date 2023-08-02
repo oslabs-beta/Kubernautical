a31c5f15e23b2a370600f5a8224739c137451f6b
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-01 22:49:48 -0500

    idk2

--- client/components/2D/Mapothy.tsx
+++ client/components/2D/Mapothy.tsx
@@ -6,20 +6,16 @@ import CytoscapeComponent from 'react-cytoscapejs';
 
 export const Mapothy = () => {
 
-    const elements = ['fuck you'];
-    const styleSheet = {};
-
     return (
-        <CytoscapeComponent
-            elements={elements}
-            stylesheet={styleSheet}
-            style={{
-                width: '100%',
-                height: '65rem',
-                border: 'solid',
-                objectFit: 'cover',
-            }}
-        ></CytoscapeComponent>
-
+        // <CytoscapeComponent
+        //     elements={elements}
+        //     stylesheet={styleSheet}
+        //     style={{
+        //         width: '100%',
+        //         height: '65rem',
+        //         border: 'solid',
+        //         objectFit: 'cover',
+        //     }}
+        // ></CytoscapeComponent>
     )
 }
