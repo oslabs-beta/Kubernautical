5583500a724008dce37548e94561d594a0713a10
Anthony <avuong22699@gmail.com>
2023-08-02 01:22:39 -0400

    Graph1

---
+++ client/containers/MainContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export default function MainContainer(){
+  return (
+    <>
+    {/* <BrowserRouter>
+     <Routes>
+      <Route index element/>
+      <Route  element/>
+     </Routes>
+    </BrowserRouter> */}
+    <div>Sup bitches </div>
+    </>
+  )
+}
