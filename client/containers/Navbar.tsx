ce67988d4c3f8b1073ba9632a4a8d87a068633f0
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-02 16:29:01 -0500

    basic styling

--- client/containers/Navbar.tsx
+++ client/containers/Navbar.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-export default function Navbar(){
-const navigate = useNavigate();
+export default function Navbar() {
+  const navigate = useNavigate();
 
-function MainDashBoard(){
-  navigate('/dashboard')
-}
-function GoHome(){
-  navigate('/');
-}
-  return(
-    <div>
-     <ul>
-      <li>
-       <button onClick={MainDashBoard}>Main Dashboard</button>
-      </li>
-      <li>
-       <button onClick={GoHome}>Cluster View</button>
-      </li>
-     </ul>
+  function MainDashBoard() {
+    navigate('/dashboard')
+  }
+  function GoHome() {
+    navigate('/');
+  }
+  return (
+    <div className='navBar'>
+      <div>NAVBAR</div>
+
+      <button className='navButton' onClick={MainDashBoard}>Main Dashboard</button>
+      <button className='navButton' onClick={GoHome}>Cluster View</button>
+
     </div>
   )
 }
