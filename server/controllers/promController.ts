1343068e72582770518214e3197c8db9bc41ea5b
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-03 13:41:19 -0500

    fixed everything

--- server/controllers/promController.ts
+++ server/controllers/promController.ts
@@ -1,36 +1,44 @@
 import type { Request, Response, NextFunction } from 'express';
-// import { promController } from '../../types';
 import type { prometheusController } from '../../types/types';
 
 
-        //modularize queries, figure out how queries even work
-        //namespace, pod, node
+//start and end total time period, step is how often to grab data point
+// [time in minutes] averages to smoothe the graph
+//sum 
+//summation
+//rate 
+//per second usage
+//100 does (changed for 2.82 (940*3/1000) cores)
+//number of cores used * 100/2.82 (cores used * 35.46)
+//how to find available cores dynamically ---- Steves next task
 
 
+const promController: prometheusController = {
 
-        //start and end total time period, step is how often to grab data point
-            // [time in minutes] averages to smoothe the graph
-        //sum 
-            //summation
-        //rate 
-            //per second usage
-        //100 does (changed for 2.82 (940*3/1000) cores)
-            //number of cores used * 100/2.82 (cores used * 35.46)
-        //how to find available cores dynamically ---- Steves next task
+    getMetrics: async (req: Request, res: Response, next: NextFunction) => {
 
-        //TODO<-------------------------------------------------------QUERIES FOR ENTIRE CLUSTER---------------------------------------------------------------->
-        if (type === 'cpu') url = `http://localhost:9090/api/v1/query_range?query=sum(rate(container_cpu_usage_seconds_total{container!=""}[5m]))*35.46&start=${start}&end=${end}&step=30m`;
-        if (type === 'mem') url = `http://localhost:9090/api/v1/query_range?query=sum(container_memory_usage_bytes{container!=""})&start=${start}&end=${end}&step=30m`;
+        //? default start, stop, step, query string
+        let start = new Date(Date.now() - 1440 * 60000).toISOString(); //24 hours
+        let end = new Date(Date.now()).toISOString();
+        let step = 10;
+        let query = 'http://localhost:9090/api/v1/query_range?query=';
+
+        const { type, hour } = req.query;
+        const userCores = 100 / res.locals.cores;
+        start = new Date(Date.now() - Number(hour) * 3600000).toISOString();
+        step = Math.ceil((step / (24 / Number(hour))));
+
+        //!<-------------------------------------------------------QUERIES FOR ENTIRE CLUSTER---------------------------------------------------------------->
+        // if (type === 'cpu') query = `sum(rate(container_cpu_usage_seconds_total{container!=""}[5m]))*35.46&start=${start}&end=${end}&step=30m`;
+        // if (type === 'mem') query = `sum(container_memory_usage_bytes{container!=""})&start=${start}&end=${end}&step=30m`;
+
+        if (type === 'cpu') query += `sum(rate(container_cpu_usage_seconds_total{container!=""}[5m]))*${userCores}&start=${start}&end=${end}&step=${step}m`;
+        if (type === 'mem') query += `sum(container_memory_usage_bytes{container!=""})&start=${start}&end=${end}&step=${step}m`;
 
         try {
-            // const response = await fetch(promURL + query);
             const response = await fetch(query);
             const data = await response.json();
-            console.log('data:', data)
-
-            //figure out what we want
             res.locals.data = data.data.result;
-
             return next();
         } catch (error) {
             return next(error);
