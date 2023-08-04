65f64f68e74330171fda440219566964d97c1850
Anthony <avuong22699@gmail.com>
2023-08-03 21:54:26 -0400

    new graphs

--- client/containers/MainDashBoard.tsx
+++ client/containers/MainDashBoard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import LineGraph from '../components/LineGraph'
+import GaugeChart from '../components/GaugeChart'
 import { FC, useState } from 'react'
 import type { Props } from '../../types/types';
 
@@ -15,13 +16,13 @@ const MainDashBoard: FC<Props> = ({ header }) => {
           title="CPU Usage"
           type="cpu"
           yAxisTitle='CPU Percentage Usage'
-          color='aqua'
+          color='rgba(0,255,255,.5)'
         />
         <LineGraph
           title="Memory Usage"
           type="mem"
           yAxisTitle='Memory Used (GB)'
-          color='orange'
+          color='rgba(0,255,0,.5)'
         />
       </div>
     </>
