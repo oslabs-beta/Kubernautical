5fec15575f4528057faa8347baad6147d9ccff52
Anthony <avuong22699@gmail.com>
2023-08-06 15:50:05 -0400

    new stuff

--- client/containers/MainDashBoard.tsx
+++ client/containers/MainDashBoard.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import LineGraph from '../components/Graphs/LineGraph'
-// import GaugeChart from '../components/GaugeChart'
+import LineGraph from '../components/LineGraph'
+import GaugeChart from '../components/GaugeChart'
 // import BarGraph from '../components/BarGraph'
 import { FC, useState } from 'react'
 import type { Props } from '../../types/types';
 
 const MainDashBoard: FC<Props> = ({ header }) => {
   return (
-
+//age of cluster
+//stacked bar chart on how much usage each namespace is taking
+//
     <>
       <div className='mainHeader'>{header}</div>
-
       <div className='miniContainerGraph'>
-      
+        <GaugeChart
+        type = "req"
+        />
         <LineGraph
           title="CPU Usage"
           type="cpu"
