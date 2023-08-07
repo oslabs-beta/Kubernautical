cc9b64f6a690cee4663dfe46a9e4b3eac33b962f
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-07 15:18:20 -0500

    scripts working on front end

--- client/containers/InvisibleNavbar.tsx
+++ client/containers/InvisibleNavbar.tsx
@@ -12,11 +12,28 @@ const InvisibleNavbar: FC<Props> = () => {
     }
 
     const Modal: FC<Props> = ({ style }) => {
+        const [vU, setVu] = useState('')
+        const [duration, setDuration] = useState('');
+        const loadtest = async () => {
+            //do we need both inputs
+            if (vU !== '' && duration === '' || vU === '' && duration !== '') return alert('Please fill out both fields');
+            try {
+                const response = await fetch(`/api/k6/test?vus=${vU}&duration=${duration}`)
+                const data = response.json()
+                setShowModal(false)
+                console.log('Load Testing')
+            } catch (error) {
+                console.log('error in running load test:', error)
+            }
+        }
         return (
             <>
                 <div className='page-mask'></div>
                 <div className='invisModal' style={{ top: style, position: 'absolute' }}>
-                    Showing
+                    <input type='number' placeholder='Number of VUs' onChange={(e) => setVu(e.target.value)} />
+                    <input type='number' placeholder='Test Duration' onChange={(e) => setDuration(e.target.value)} />
+                    <button onClick={loadtest}>DDOS me</button>
+                    <button onClick={() => setShowModal(false)} >X</button>
                 </div>
             </>
         )
