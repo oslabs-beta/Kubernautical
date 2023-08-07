63bb2d4edcd6ac8fd5eab2945c3811fcc8de862c
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-07 12:25:38 -0500

    modal is set up

--- client/containers/InvisibleNavbar.tsx
+++ client/containers/InvisibleNavbar.tsx
@@ -1,12 +1,30 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, SyntheticEvent, CSSProperties } from 'react'
 import type { Props } from '../../types/types';
 
+// let defaultCSS : CSSProperties;
 const InvisibleNavbar: FC<Props> = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [modalPos, setModalPos] = useState(0);
 
+    const openModal = (e: SyntheticEvent) => {
+        setModalPos(e.currentTarget.getBoundingClientRect().bottom + 5);
+        showModal ? setShowModal(false) : setShowModal(true);
+    }
 
+    const Modal: FC<Props> = ({ style }) => {
+        return (
+            <>
+                <div className='page-mask'></div>
+                <div className='invisModal' style={{ top: style, position: 'absolute' }}>
+                    Showing
+                </div>
+            </>
+        )
+    }
     return (
         <>
-            <button className='invisNavButton'>Load Testing</button>
+            <button onClick={(e) => openModal(e)} className='invisNavButton'>Load Testing</button>
+            {showModal ? <Modal style={modalPos} /> : <div></div>}
         </>
 
     )
