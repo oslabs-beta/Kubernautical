b66a907cefd0334833068638c67ab12a28b4642d
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-07 16:26:36 -0500

    merged

--- client/containers/InvisibleNavbar.tsx
+++ client/containers/InvisibleNavbar.tsx
@@ -1,27 +1,25 @@
 import React, { FC, useState, SyntheticEvent, CSSProperties, useEffect } from 'react'
 import type { Props } from '../../types/types';
-import loadingImage from '../assets/images/network.png'
+import { GlobalContext } from '../components/Contexts';
 
 // let defaultCSS : CSSProperties;
 const InvisibleNavbar: FC<Props> = () => {
     const [showModal, setShowModal] = useState(false);
     const [modalPos, setModalPos] = useState(0);
-    const [vU ,setVu]= useState(0)
-    const [duration, setDuration ]= useState(0)
 
     const openModal = (e: SyntheticEvent) => {
         setModalPos(e.currentTarget.getBoundingClientRect().bottom + 5);
         showModal ? setShowModal(false) : setShowModal(true);
     }
-    const loadtest = async () =>{
+    const loadtest = async () => {
         try {
             const response = await fetch('/api/k6/test')
             const data = response.json()
             setShowModal(false)
             console.log('Load Testing')
-            
+
         } catch (error) {
-            console.log('error in running load test:',error)
+            console.log('error in running load test:', error)
         }
     }
 
@@ -38,33 +36,31 @@ const InvisibleNavbar: FC<Props> = () => {
                 const response = await fetch(`/api/k6/test?vus=${vU}&duration=${duration}&ip=${service}`)
                 const data = response.json()
                 setShowModal(false)
-                setLoading(true)
                 console.log('Load Testing')
             } catch (error) {
                 console.log('error in running load test:', error)
             }
         }
-  
-        const toMilliseconds = Number(duration)* 1000
-
-        useEffect(()=>{
-            console.log('hello')
-            setTimeout(()=>{
-                setLoading(false)
-            },toMilliseconds)
-        },[loading])
 
         const imgSrc = '../assets/images/network.png'
         return (
             <>
                 <div className='page-mask'></div>
                 <div className='invisModal' style={{ top: style, position: 'absolute' }}>
-                    <label>How many Users for Test?</label>
-                    <input type = 'number' onChange = {(e)=> setVu(Number(e.target.value))}/>
-                    <label>How long to run the Test</label>
-                    <input type = 'number' onChange = {(e)=> setDuration(Number(e.target.value))}/>
-                    <button onClick = {loadtest}>DDOS me</button>
-                    <button onClick = {() => setShowModal(false)} >X</button>
+                    <div>
+                        <select className='containerButton mapButton' value={service} onChange={(e) => setService(e.target.value)}>
+                            <option value=''>Select Service</option>
+                            {globalServices ? globalServices.map((el) => {
+                                return (
+                                    <option key={uuidv4()} value={el.ip}>{el.name}</option>
+                                )
+                            }) : <div></div>}
+                        </select>
+                    </div>
+                    <input className='InvisInput' type='number' placeholder='Number of VUs' onChange={(e) => setVu(e.target.value)} />
+                    <input className='InvisInput' type='number' placeholder='Test Duration' onChange={(e) => setDuration(e.target.value)} />
+                    <button className='InvisSubmit' onClick={loadtest}>DDOS me</button>
+                    <button className='closeInvisModal' onClick={() => setShowModal(false)} >X</button>
                 </div>
             </>
         )
