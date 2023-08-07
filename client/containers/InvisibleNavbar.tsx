c78d00aa78c57161f9b406fff9e25c982947e15c
Anthony <avuong22699@gmail.com>
2023-08-07 15:36:09 -0400

    update button

--- client/containers/InvisibleNavbar.tsx
+++ client/containers/InvisibleNavbar.tsx
@@ -5,10 +5,23 @@ import type { Props } from '../../types/types';
 const InvisibleNavbar: FC<Props> = () => {
     const [showModal, setShowModal] = useState(false);
     const [modalPos, setModalPos] = useState(0);
+    const [vU ,setVu]= useState(0)
+    const [duration, setDuration ]= useState(0)
 
     const openModal = (e: SyntheticEvent) => {
         setModalPos(e.currentTarget.getBoundingClientRect().bottom + 5);
         showModal ? setShowModal(false) : setShowModal(true);
+    }
+    const loadtest = async () =>{
+        try {
+            const response = await fetch('/api/k6/test')
+            const data = response.json()
+            setShowModal(false)
+            console.log('Load Testing')
+            
+        } catch (error) {
+            console.log('error in running load test:',error)
+        }
     }
 
     const Modal: FC<Props> = ({ style }) => {
@@ -16,7 +29,12 @@ const InvisibleNavbar: FC<Props> = () => {
             <>
                 <div className='page-mask'></div>
                 <div className='invisModal' style={{ top: style, position: 'absolute' }}>
-                    Showing
+                    <label>How many Users for Test?</label>
+                    <input type = 'number' onChange = {(e)=> setVu(Number(e.target.value))}/>
+                    <label>How long to run the Test</label>
+                    <input type = 'number' onChange = {(e)=> setDuration(Number(e.target.value))}/>
+                    <button onClick = {loadtest}>DDOS me</button>
+                    <button onClick = {() => setShowModal(false)} >X</button>
                 </div>
             </>
         )
