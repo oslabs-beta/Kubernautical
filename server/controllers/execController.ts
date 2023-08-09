4f6dbafc6405d0b806a47f690a4276dd1038e026
STAC98 <117822601+STAC98@users.noreply.github.com>
2023-08-09 12:24:31 -0700

    working on crud for deployments

--- server/controllers/execController.ts
+++ server/controllers/execController.ts
@@ -15,49 +15,14 @@ const execController: execController = {
           }
           console.log(stdout);
         });
-       } //else if (deployment) {
-
-      // } else if (service)
+       } 
 
       return next();
     } catch (err) {
       return next(err);
     }
   },
-  // deployment: async (req: Request, res: Response, next: NextFunction) => {
-  //   const { namespace, crud, deployment, service } = req.query;
-  //   try {
-  //     const command = `kubectl ${crud} namespace ${namespace}`;
-  //     exec(command, (err, stdout, stderr) => {
-  //       if (err) {
-  //         console.log('Error executing command:', err);
-  //         throw new Error();
-  //       }
-  //       console.log(stdout);
-  //       return next();
 
-  //     });
-  //   } catch (err) {
-  //     return next(err);
-  //   }
-  // },
-  // service: async (req: Request, res: Response, next: NextFunction) => {
-  //   const { namespace, crud } = req.query;
-  //   try {
-  //     const command = `kubectl ${crud} namespace ${namespace}`;
-  //     exec(command, (err, stdout, stderr) => {
-  //       if (err) {
-  //         console.log('Error executing command:', err);
-  //         throw new Error();
-  //       }
-  //       console.log(stdout);
-  //       return next();
-
-  //     });
-  //   } catch (err) {
-  //     return next(err);
-  //   }
-  // }
 };
 
 export default execController;
