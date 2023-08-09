975b1537c2e31ac5a675ad14aa9dcc9012870092
STAC98 <117822601+STAC98@users.noreply.github.com>
2023-08-08 21:14:52 -0700

    delete for namespaces, exec

--- server/controllers/execController.ts
+++ server/controllers/execController.ts
@@ -5,6 +5,43 @@ import { exec, execFile, execSync } from 'child_process';
 const execController: execController = {
     add: async (req: Request, res: Response, next: NextFunction) => {
         const { namespace } = req.query;
+
+        try {
+          const command = `kubectl create namespace ${namespace}`;
+          exec(command, (err, stdout, stderr) => {
+                if (err) {
+                  console.log('Error executing command:', err);
+                    throw new Error();
+                }
+                console.log('stdout:', stdout);
+                return next();
+            });
+        } catch (err) {
+          return next(err);
+        }
+    },
+    delete: async (req: Request, res: Response, next: NextFunction) => {
+      const { namespace } = req.query;
+
+      try {
+        const command = `kubectl delete namespace ${namespace}`;
+        exec(command, (err, stdout, stderr) => {
+              if (err) {
+                console.log('Error executing command:', err);
+                  throw new Error();
+              }
+              console.log('stdout:', stdout);
+              return next();
+          });
+      } catch (err) {
+        return next(err);
+      }
+    }
+};
+
+export default execController;
+
+
         // try {
         //   console.log('hi')
         //   const command = `kubectl get namespaces`;
@@ -22,22 +59,3 @@ const execController: execController = {
         // } catch (err) {
         //     return next(err);
         // }
-        try {
-          const command = `kubectl create namespace ${namespace}`;
-          exec(command, (err, stdout, stderr) => {
-                if (err) {
-                  console.log('Error executing command:', err);
-                    throw new Error();
-                }
-                console.log('stdout:', err);
-                // res.locals.namespaces = stdout;
-                console.log('stdout:', stdout);
-                return next();
-            });
-        } catch (err) {
-          return next(err);
-        }
-    }
-};
-
-export default execController;
