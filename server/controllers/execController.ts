a868f192caf6502c72869ecf1c435a1e5a1c4895
STAC98 <117822601+STAC98@users.noreply.github.com>
2023-08-09 16:51:39 -0700

    working on exec for deployment-replicas

--- server/controllers/execController.ts
+++ server/controllers/execController.ts
@@ -20,7 +20,9 @@ const execController: execController = {
     }
   },
   deployment: async (req: Request, res: Response, next: NextFunction) => {
+
     const { namespace, crud, image, deployment, replicas, type, port, targetPort } = req.query;
+    // http://localhost:3000/api/exec/dep?namespace=test69&crud=create&image=swaggerapi/petstore3&deployment=test69deployment
     try {
       // kubectl expose deployment <deployment-name> --port=x --target-port=x --type=LoadBalancer
       // kubectl scale deployment <deployment-name> --replicas=x -n <namespace>
@@ -30,14 +32,18 @@ const execController: execController = {
       let action = ``;
       if (crud === `create`) action = `--image=${image}`;
       if (crud === `scale`) action = `--replicas=${replicas}`;
-      if (crud === `delete`) action = `--port=${port} --target-port=${targetPort} --type=${type}`; 
+      // if (crud === `delete`) action = `--port=${port} --target-port=${targetPort} --type=${type}`; 
+      // if (crud === `expose`) action = `--port=${port} --target-port=${targetPort} --type=${type}`;
+
+      console.log('action:', action)
       const command = `kubectl ${crud} deployment ${deployment} ${action ? `${action}` : ''} -n ${namespace}`;
+      console.log('command:', command)
       exec(command, (err, stdout, stderr) => {
         if (err) {
           console.log('Error executing command:', err);
           throw new Error();
         }
-        console.log(stdout);
+        console.log(`stdout:`,stdout);
         return next();
       });
     } catch (err) {
