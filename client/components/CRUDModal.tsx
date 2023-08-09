8651e77691ab2ffdc0e94859ef67dfe92a221393
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-08 23:50:44 -0500

    more crud fixes

--- client/components/CRUDModal.tsx
+++ client/components/CRUDModal.tsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState, useContext, FC, SyntheticEvent } from 'reac
 import { ClusterData, CLusterObj, Props } from '../../types/types';
 import { GlobalContext } from './Contexts';
 import { v4 as uuidv4 } from 'uuid';
+import { error } from 'console';
 
 const CRUDModal: FC<ClusterData> = () => {
   const { setGlobalNamesapces, globalNamespaces,
     setGlobalServices, globalServices,
-    globalClusterData, setGlobalClusterData,
-    setShowEditModal
+    setGlobalClusterData, globalClusterData,
+    setShowEditModal,
+    setGlobalCrudChange, globalCrudChange
   } = useContext(GlobalContext);
   const [ns, setNs] = useState('');
   const [service, setService] = useState('');
@@ -19,16 +21,39 @@ const CRUDModal: FC<ClusterData> = () => {
     //TODO yeah this doesnt work ver well i hate it
     //TODO figure out global re renders for mapothy (global state)
     const [nsInner, setNsInner] = useState('');
-    const name = modalType.slice(3) === 'service' ? service : ns ? ns : null;
-    const innerText = modalType.slice(0, 3) === 'add' ? 'Enter namespace here' : name ? `Are you sure you want to remove ${name}?` : 'Please make a selection';
+    const name = modalType.slice(4) === 'service' ? service : ns ? ns : null;
+    const innerText = modalType.slice(0, 4) === 'add' ? 'Enter namespace here' : name ? `Are you sure you want to remove ${name}?` : 'Please make a selection';
+
+    const crudFunction = async () => { //? hard coded trash
+      try {
+        if (nsInner === '') return alert('Please fill out field')
+        const response = await fetch(`api/exec/ns?namespace=${nsInner}`)
+        setGlobalCrudChange ? globalCrudChange ? setGlobalCrudChange(false) : setGlobalCrudChange(true) : null;
+        if (!response.ok) throw new Error();
+      } catch (error) {
+        throw error;
+      }
+    }
+    const crudDelete = async () => { //? hard coded trash
+      try {
+        const response = await fetch(`api/exec/delete?namespace=${ns}`)
+        setGlobalCrudChange ? globalCrudChange ? setGlobalCrudChange(false) : setGlobalCrudChange(true) : null;
+        if (!response.ok) throw new Error();
+      } catch (error) {
+        throw error;
+      }
+    }
     return (
       <>
         <div className='page-mask'></div>
         <div className='invisModal' style={{ top: style, position: 'absolute' }}>
           <div>{innerText}</div>
           {modalType.slice(0, 3) === 'add' ?
-            <input className='InvisInput' type='text' placeholder='New Namespace' onChange={(e) => setNsInner(e.target.value)} /> : null}
-          <button className='InvisSubmit' onClick={() => { setNs(nsInner); setShowModal(false); setShowEditModal ? setShowEditModal(false) : null }}>Finalize</button>
+            <>
+              <input className='InvisInput' type='text' placeholder='New Namespace' onChange={(e) => setNsInner(e.target.value)} />
+              <button className='InvisSubmit' onClick={() => { crudFunction(); setShowEditModal ? setShowEditModal(false) : null; }}>Finalize</button>
+            </>
+            : <button className='InvisSubmit' onClick={() => { crudDelete(); setShowEditModal ? setShowEditModal(false) : null }}>Finalize</button>}
           <button className='closeInvisModal' onClick={() => setShowModal(false)} >X</button>
         </div>
       </>
