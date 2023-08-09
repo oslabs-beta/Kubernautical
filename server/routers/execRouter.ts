169e5f3df5c69a47a2de046436237b79cd9bf427
STAC98 <117822601+STAC98@users.noreply.github.com>
2023-08-09 12:24:31 -0700

    working on crud for deployments

--- server/routers/execRouter.ts
+++ server/routers/execRouter.ts
@@ -8,13 +8,7 @@ router.get('/', execController.crud, (req: Request, res: Response, next: NextFun
     res.status(200).json(res.locals.namespaces);
 });
 
-// router.get('/dep', execController.deployment, (req: Request, res: Response, next: NextFunction) => {
-//     res.status(200).json(res.locals.namespaces);
-// });
 
-// router.get('/serv', execController.service, (req: Request, res: Response, next: NextFunction) => {
-//     res.status(200).json(res.locals.namespaces);
-// });
 
 
 export default router;
