8918b1ad8278ad50ce442c002a1f27a268a8763b
Anthony <avuong22699@gmail.com>
2023-08-09 00:55:24 -0400

    more testing

--- cypress/e2e/frontEndTest.cy.ts
+++ cypress/e2e/frontEndTest.cy.ts
@@ -1,10 +1,46 @@
 ///<reference types = 'cypress'/>
 import Chance from 'chance'
 const chance = new Chance()
+import {mount} from 'cypress/react'
+import LineGraph from '../../client/components/Graphs/LineGraph'
+import GaugeChart from '../../client/components/Graphs/GaugeChart'
+import InvisibleNavbar from '../../client/containers/InvisibleNavbar'
+
 
 const fakeNumber = chance.integer({min:0,max:20}).toString()
-describe('Main DashBoard Monitor', () => {
+const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/
+
+//REACT COMPONENTS TESTING
+// describe('<LineGraph>',()=>{
+//     it('mounts',()=>{
+    
+//     })
+
+// })
+// describe('<InvisibleNavbar>',()=>{
+//   it('mounts',()=>{
   
+//   })
+
+// })
+// describe('<GaugeChart>',()=>{
+//   it('mounts',()=>{
+  
+//   })
+
+// })
+// describe('ClusterView Test', () => {
+//   beforeEach(()=>{
+//     Cypress.on('uncaught:exception', (err) => {
+//       if (resizeObserverLoopErrRe.test(err.message)) {
+//           return false
+//       }
+//   })
+//     cy.visit('http://localhost:8000/')
+//   })
+//   it('Selecting Namespaces should work')
+// })
+describe('Main DashBoard Monitor test', () => {
   beforeEach(()=>{
     cy.visit('http://localhost:8000/dashboard')
   })
@@ -21,41 +57,58 @@ describe('Main DashBoard Monitor', () => {
       expect(alertText).to.equal('Please fill out both fields');
     });
   })
-  it('Navigation to Network Performance works',()=>{
-    cy.get('#NetworkButton').click()
-    cy.url().should('include','network')
-  })
-  it('Changing time should work', ()=>{
-    cy.get('#hourDropDown').select('1')
-    cy.contains('1 hour').should("be.visible")
-    cy.get('#hourDropDown').select('6')
-    cy.contains('6 hour').should("be.visible")
-    cy.get('#hourDropDown').select('12')
-    cy.contains('12 hour').should("be.visible")
-    cy.get('#hourDropDown').select('24')
-    cy.contains('24 hour').should("be.visible")
-  })
 })
-describe('Network Performance Monitor', () => {
+
+describe('Network Performance Monitor test', () => {
   beforeEach(()=>{
     cy.visit('http://localhost:8000/network')
   })
   it('should contain the title Network Performance Monitor',()=>{
     cy.contains('Network Performance Monitor').should("be.visible")
   })
+})
+
+describe('NavBar test', () => {
+  beforeEach(()=>{
+    Cypress.on('uncaught:exception', (err) => {
+      if (resizeObserverLoopErrRe.test(err.message)) {
+          return false
+      }
+  })
+    cy.visit('http://localhost:8000/')
+  })
   it('Navigation to Main DashBoard works',()=>{
-    cy.get('#MainDashButton').click()
+    cy.get('#MainDashButton').click({force:true})
     cy.url().should('include','dashboard')
   })
-  it('Changing time should work', ()=>{
-    cy.get('#hourDropDown').select('1')
-    cy.contains('1 hour').should("be.visible")
-    cy.get('#hourDropDown').select('6')
-    cy.contains('6 hour').should("be.visible")
-    cy.get('#hourDropDown').select('12')
-    cy.contains('12 hour').should("be.visible")
-    cy.get('#hourDropDown').select('24')
-    cy.contains('24 hour').should("be.visible")
+  it('Navigation to Network Performance works',()=>{
+    cy.get('#NetworkButton').click({force:true})
+    cy.url().should('include','network')
   })
-
+  it('Edit Cluster should work',()=>{
+    cy.get('#EditClusterButton').click({force:true})
+    cy.contains('Select Namespace')
+  })
 })
+
+describe('LineGraph Test',()=>{
+  beforeEach(()=>{
+    cy.visit('http://localhost:8000/dashboard')
+  })
+    it('Hour Drop Down Menu exists', ()=>{
+      cy.get('#hourDropDown').select('1')
+      cy.contains('1 hour').should('have.value', '1')
+      cy.get('#hourDropDown').select('6')
+      cy.contains('6 hour').should('have.value', '6')
+      cy.get('#hourDropDown').select('12')
+      cy.contains('12 hour').should('have.value', '12')
+      cy.get('#hourDropDown').select('24')
+      cy.contains('24 hour').should('have.value', '24')
+    })
+    // it('Changing Name Spaces Menu exists')
+})
+
+// describe('Edit Cluster View Modal works',()=>{
+//   it('Adding Namespace should work')
+//   it('Deleting Namespace should work')
+// })
