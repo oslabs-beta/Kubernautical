0c9fb119741904f911a5ef3269ffa4a78faf1568
Anthony <avuong22699@gmail.com>
2023-08-09 20:54:43 -0400

    E2E test Update

---
+++ cypress/components/LineGraph.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import LineGraph from '../../client/components/Graphs/LineGraph'
+
+describe('<LineGraph />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<LineGraph />)
+  })
+})
