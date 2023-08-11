7035adc64a6a658759c4d922914e9026ed183a22
STAC98 <117822601+STAC98@users.noreply.github.com>
2023-08-11 08:24:49 -0700

    working on backend testing for routes

--- jest/server.test.ts
+++ jest/server.test.ts
@@ -3,36 +3,75 @@
 // import Request from "supertest";
 // import assert from "assert";
 // import { exec, ChildProcess } from 'child_process';
-import { expect } from '@jest/globals'; // Import expect from Jest
+import { expect } from '@jest/globals'; 
 import supertest from 'supertest';
 import express from 'express';
+import { is } from 'cypress/types/bluebird';
 
 
-const app = express(); // Replace with your Express app
+const app = express(); 
 const request = supertest(app);
 
 
-describe("Error catch route", () => {
-  it("should respond with a 404 error", async () => {
-    const response = await request.get("/wrongaddress");
-    expect(response.status).toBe(404);
+// router.get('/namespaces', clusterController.getAllNamespaces, (req: Request, res: Response, next: NextFunction) => {
+//   res.status(200).json(res.locals.namespaces);
+// });
+// router.get('/pods', clusterController.getAllPods, (req: Request, res: Response, next: NextFunction) => {
+//   res.status(200).json(res.locals.pods);
+// });
+// router.get('/nodes', clusterController.getAllNodes, (req: Request, res: Response, next: NextFunction) => {
+//   res.status(200).json(res.locals.nodes);
+// });
+// // router.get('/logs', clusterController.getAllPods, clusterController.getAllPodLogs, (req: Request, res: Response, next: NextFunction) => {
+// //     res.status(200).json(res.locals.logs);
+// // });
+
+// //!---------------------------------------------These Routes are for testing (for now)------------------------------------->
+// router.get('/services', clusterController.getAllServices, (req: Request, res: Response, next: NextFunction) => {
+//   res.status(200).json(res.locals.services);
+// });
+// router.get('/deployments', clusterController.getAllDeployments, (req: Request, res: Response, next: NextFunction) => {
+//   res.status(200).json(res.locals.deployments);
+// });
+
+
+// });
+describe('404 error handler', () => {
+  it('should respond with a 404 status code for unknown API route', async () => {
+    const res = await request.get('/api/non-existent-route'); 
+    expect(res.status).toBe(404);
   });
 });
 
+describe('API Routes', () => {
+  it("should respond with namespaces data", async () => {
+    const res = await request.get('/namespaces');
+    console.log('Response:', res.status, res.body);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([]);
+  })
+});
 
 
-// const server = "http://localhost:3001";
-// let serverProcess: ChildProcess;
+// import clusterRouter from '../server/routers/clusterRouter';
+// import clusterController from '../server/controllers/clusterController';
 
-// beforeAll(async () => {
-//   console.log('Starting the server...');
-//   const startServer = 'npm run serve:dev';
-//   serverProcess = exec(startServer);
-//   await new Promise((resolve) => setTimeout(resolve, 2000)); // Wait for the server to start (adjust the delay as needed)
+
+// jest.mock('./', () => ({
+//   listNamespace: jest.fn(() => Promise.resolve({ body: { items: [] } }))
+// }));
+
+// describe('Cluster Routes - Namespaces', () => {
+//   beforeAll(() => {
+//     app.use('/cluster', clusterRouter);
+//   });
+
+//   it('should respond with namespaces data', async () => {
+//     const response = await request.get('/cluster/namespaces');
+//     expect(response.status).toBe(200);
+//     expect(response.body).toEqual([]);
+//   });
 // });
 
-// afterAll(() => {
-//   console.log('Stopping the server...');
-//   serverProcess.kill(); // Terminate the server process (you may need to customize this logic to gracefully stop your server)
-// });
+
 
