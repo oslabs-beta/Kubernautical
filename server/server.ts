8d817397d84595318277899ea3f9eb25c29f1c41
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-13 22:50:05 -0500

    fixed loki

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,12 @@
 import express from 'express';
 import type { Request, Response, NextFunction } from 'express';
 import type { ServerError } from '../types/types';
-import path from 'path';
 import apiRouter from './routers/apiRouter';
-
 
 const PORT = 3000;
 
-
 const app = express();
 app.use(express.json());
-// app.use(express.static(path.resolve(__dirname, '../client')));
-
-//add routes
 
 app.use('/api', apiRouter);
 
@@ -36,22 +30,4 @@ app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
 
-
-
-
-
-
-// total mem cluster
-    //`http://localhost:9090/api/v1/query_range?query=sum(container_memory_usage_bytes)&start=${start}&end=${end}&step=10m`
-// total cpu cluster
-    //`http://localhost:9090/api/v1/query_range?query=sum(rate(container_cpu_usage_seconds_total[10m]))*100&start=${start}&end=${end}&step=10m`
-
-
-//node transmit cluster level
-    //`http://localhost:9090/api/v1/query_range?query=sum(rate(node_network_transmit_bytes_total[10m]))&start=${start}&end=${end}&step=10m`
-//node recieve cluster level
-    //`http://localhost:9090/api/v1/query_range?query=sum(rate(node_network_receive_bytes_total[10m]))&start=${start}&end=${end}&step=10m`
-
-
-// export const start = new Date(Date.now() - 1440 * 60000).toISOString(); //24 hours
-// export const end = new Date(Date.now()).toISOString();
+export default app;
