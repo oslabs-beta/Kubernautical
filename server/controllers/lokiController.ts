8341ec81cd17b90763c0752f9a97e4e47ea149c7
STAC98 <117822601+STAC98@users.noreply.github.com>
2023-08-13 18:32:39 -0700

    logs for errors, info, namespaces

--- server/controllers/lokiController.ts
+++ server/controllers/lokiController.ts
@@ -1,5 +1,7 @@
 import type { Request, Response, NextFunction } from 'express';
 import type { lokiController } from '../../types/types';
+import { result } from 'cypress/types/lodash';
+import { any } from 'cypress/types/bluebird';
 
 
 
@@ -9,18 +11,62 @@ const lokiController: lokiController = {
   testing: async (req: Request, res: Response, next: NextFunction) => {
     
     try {
+      // {namespace="loki"} |= "error" != "info"
+      const {namespace, limit, start, end, log} = req.query
       console.log('hi from loki controller');
 
-      const lokiEndpoint = 'http://34.30.165.202';
-      const logQuery = `{namespace="petstore3"}`
+      const lokiEndpoint = 'http://34.30.165.202/loki/api/v1/query_range?query=';
+      let logQuery = ``
+      
+      if (namespace) logQuery = `{namespace="${namespace}"}`;
+      if (start && end) logQuery += `{time >= ${start} and time <= ${end}}`;
+      if (log === 'error') { 
+        // if (logQuery) logQuery += ', ';
+        // logQuery += `{level=~"${log}"}`;
+        // // logQuery += `{level="level=${log}"}`;
+        logQuery += ` |= "${log}" != "info"`
+      }
+      if (log === 'info') { 
+        // if (logQuery) logQuery += ', ';
+        // logQuery += `{level=~"${log}"}`;
+        // // logQuery += `{level="level=${log}"}`;
+        logQuery += ` |= "${log}" != "error"`
+      }
+      if (limit) logQuery += `&limit=${limit}`;
+    
  
-      const response = await fetch(`${lokiEndpoint}/loki/api/v1/query_range?query=${encodeURIComponent(logQuery)}&limit=100`);
+      const query = lokiEndpoint + logQuery;
+      console.log('query:', query);
+      
+      const response = await fetch(query);
       const data = await response.json();
 
+      let result = data.data.result;
 
-      console.log('data:', response);
+      const logs: any[] = [];
+
+      result.forEach((object: any) => {
+        const stream = object.stream;
+        const values = object.values;
+
   
-      res.locals.data = data;
+        values.forEach((value: any) => {
+          const log = {
+            timestamp: value[0],
+            message: value[1],
+            namespace: stream.namespace, 
+            container: stream.container,
+            pod: stream.pod,
+            job: stream.job,
+          };
+
+          logs.push(log);
+        });
+      });
+
+      console.log('logs:', logs);
+
+      res.locals.data = logs;
   
       return next();
     } catch (error) {
