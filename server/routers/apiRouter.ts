41898225ecc3eefdcc32ee9683c3f3801032db4b
STAC98 <117822601+STAC98@users.noreply.github.com>
2023-08-17 12:43:33 -0700

    added comments to backend routes

--- server/routers/apiRouter.ts
+++ server/routers/apiRouter.ts
@@ -5,10 +5,10 @@ import mapRouter from './mapRouter';
 import k6Router from './k6Router';
 import crudRouter from './crudRouter';
 import lokiRouter from './lokiRouter';
+
 const router = express.Router();
 
-
-
+// routes for various functional parts of the application
 router.use('/prom', promRouter);
 router.use('/cluster', clusterRouter);
 router.use('/map', mapRouter);
